4efcf089c6f955249c74bcb971a6f14137128760
Aidan Spendlove <aidancspendlove@gmail.com>
2026-01-18 14:40:47 -0700

    allow uploading files

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
@@ -8,6 +13,19 @@ func main() {
 	router.Static("/static", "./static")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", nil)
+	})
+	router.POST("/submit", func(c *gin.Context) {
+		file, err := c.FormFile("pdf")
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+		fmt.Printf("Filename: %s, Size: %d bytes", file.Filename, file.Size)
+
+		dst := "./uploads/" + file.Filename
+		c.SaveUploadedFile(file, dst)
+
+		c.String(http.StatusOK, "File %s uploaded successfully", file.Filename)
 	})
 	router.Run()
 }
